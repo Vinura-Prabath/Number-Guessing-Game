e47bfd620a85f9b98d44681988d24465bfb79cfd
Vinura-Prabath <vinurakumarathunga8866@gmail.com>
2026-07-18 10:09:48 +0530

    Refactor guess checking logic and enhance user feedback with success message

--- app.js
+++ app.js
@@ -3,9 +3,27 @@ let randomNumber = Math.floor(Math.random() * 10) + 1;
 
 function checkGuess() {
 
-   Swal.fire({
-  title: "Good job!",
-  text: "You clicked the button!",
-  icon: "success"
-});
+    let guess = Number(document.getElementById("guess").value);
+    let result = document.getElementById("result");
+
+    if (guess < 1 || guess > 10) {
+        result.innerHTML = "Please enter a number between 1 and 10.";
+    }
+    else if (guess > randomNumber) {
+        result.innerHTML = "📈 Too High! Try a smaller number.";
+    }
+    else if (guess < randomNumber) {
+        result.innerHTML = "📉 Too Low! Try a bigger number.";
+    }
+    else {
+        Swal.fire({
+        title: "🎊🎉Good job 🎉🎊",
+        text: "You Guessed the Number!",
+        icon: "success"
+    });
+
+        // Generate a new random number
+        randomNumber = Math.floor(Math.random() * 10) + 1;
+    }
+    
 }
