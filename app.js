ca0058254de71e3b4ca07622bd9cf6ef722e10ec
Vinura-Prabath <vinurakumarathunga8866@gmail.com>
2026-07-18 09:34:27 +0530

    Implement number guessing game logic and update HTML structure

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("Hello World!");
+// Generate a random number between 1 and 10
+let randomNumber = Math.floor(Math.random() * 10) + 1;
+
+function checkGuess() {
+
+    let guess = Number(document.getElementById("guess").value);
+    let result = document.getElementById("result");
+
+    if (guess < 1 || guess > 10) {
+        result.innerHTML = "Please enter a number between 1 and 10.";
+    }
+    else if (guess > randomNumber) {
+        result.innerHTML = "📈 Too High! Try a smaller number.";
+    }
+    else if (guess < randomNumber) {
+        result.innerHTML = "📉 Too Low! Try a bigger number.";
+    }
+    else {
+        result.innerHTML = "🎉 Correct! You guessed the number.";
+
+        // Generate a new random number
+        randomNumber = Math.floor(Math.random() * 10) + 1;
+    }
+}
